369ab3b74a5e59baf6c97ae07048bdd7d169249d
Jason Humphrey <sjasonhumphrey@yahoo.com>
2021-06-17 11:18:28 -0500

    added files from J Mac and reset redirect

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
-//import express
+////import express
 const express = require('express');
-
 //invoke the express router 
 const router = express.Router();
-
 router.get('/', (req, res) => {
-    res.render('room')
+    res.render('registration')
 })
 //export out to main application 
 module.exports = router;
--- routes/login.js
+++ routes/login.js
@@ -4,150 +4,103 @@ const db = require('../models');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-
 router.use(express.urlencoded({extended: false}));
 router.use(express.json());
-
 console.log(db.users)
-
-
-
-
-
-
-
-router.get("/registration", async(req, res) => {
+router.get("/", async(req, res) => {
     let records = await db.regions.findAll();
-
     records.forEach(record => {
       console.log(record.dataValues.name)
   })
     // console.log(regions)
-    
     res.render('registration', {
             records: records
     });
 });
 //form submitted
 router.post('/registration', async (req, res)=>{
-
   try{
     //get information from header 
     let {username, password, email, regionID} = req.body;
-
     //hash our password 
     let passwordEncrypted = bcrypt.hashSync(password, 8);
-
     //store username, password, email inside database
-
     let result = await db.users.create({
       username: username, 
       password: passwordEncrypted, 
       email: email, 
       regionID: 2
     })
-
     res.redirect('/login')
-    
   }
   catch(error){
     res.send(error)
   }
 })
-
 module.exports = router;
-
-
-
-
-
-router.get("/login", (req, res) => {
-  res.render('login')
+router.get("/player", (req, res) => {
+    res.render('player')
 });
-
 router.get('/logout', (req, res) => {
   req.logout();  //clears the session.  
-
   res.redirect('/');
-
 })
-
 passport.use(new LocalStrategy( async (username, password, done)=>{
-
+  console.log('callinglocalstrategy')
   try{
     // make db call to check if username is in our db
-
     let records = await db.users.findAll({where: {username: username}});
     //[{}]
-
     if(records != null){
       // check passwords
       let record = records[0];
-
-      bcrypt.compare(password, record.password, (err, response)=>{
-
+      console.log(record.dataValues.password)
+      bcrypt.compare(password, record.dataValues.password, (err, response)=>{
           if(response){
+            console.log('passwordmatch')
             //this means a match, user has correct password
-            done(null, {id: record.id, username: record.username})
+            done(null, {id: record.id, username: record.dataValues.username})
           }
           else{
             //passwords didn't match
+            console.log('passwordmissmatch')
             done(null, false)
           }
       })
-
     }
     else{
       //user wasn't found in our db
       done(null, false)
     }
-
     //if user is in db, then check if password is valid
-
     // done(null, {})
-
     // done(null, false)
   }
   catch(error){
-
   }
-
 }))
-
-router.post('/login', passport.authenticate('local', {failureRedirect: '/login'}), 
+router.post('/login', passport.authenticate('local', {failureRedirect: '/'}), 
 (req, res) => {
-  
   console.log(`inside of login ${req.isAuthenticated()}`);
-  res.send('you made it through')
-
+  res.redirect('player')
 })
-
 passport.serializeUser((user, done)=>{
-
   //passport is adding information to the sessions {id:"", username:""}
-
   done(null, user.id)
 })
-
 passport.deserializeUser(async (id, done)=>{
   console.log('deserializing user');
   done(null, id)
   // try{
-
   //   //additional security 
   //   //checkign to see if user is valid with the cookie that was passed 
   //   // form the request
   //   // id is coming from the session
   //     let user = await db.users.findByPk(id);
-
   //     done(null, user)
   // }
   // catch(error){
   //   done(null, false)
   // }
-  
 })
-
-
-
 module.exports = router;
